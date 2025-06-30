cd52656549bd91557546832365104f1499ef1170
ronc <dotm@qq.com>
2025-06-30 14:39:53 +0800

    add comments

--- no-tags/no-tags.sh
+++ no-tags/no-tags.sh
@@ -7,7 +7,7 @@ cd $working_dir || exit
 
 [ -d "$puts" ] && rm -f ${puts}/* || mkdir $puts
 
-AWSIds=(00063769)
+# 客户侧 账号 ID 列表
 AWSIds=(00064895 00063769 00062925 00062828 00031356 00031812 00061708 00063644 00030998 00031295 00065224 00065522 00065437)
 
 
@@ -17,8 +17,9 @@ gen_output() {
 		exit
 	}
 
+	# 获取临时权限
 	temp_creds=$(aws sts assume-role \
-                  --role-arn "arn:aws:iam::${1}:role/YCloud-APN-RO" \
+                  --role-arn "arn:aws:iam::${1}:role/YResourceExplorerQueryRole" \
                   --role-session-name Ycloud-Session \
                   --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' \
                   --output text)
@@ -27,6 +28,7 @@ gen_output() {
 	export AWS_SECRET_ACCESS_KEY=$(echo "$temp_creds" | awk '{print $2}')
 	export AWS_SESSION_TOKEN=$(echo "$temp_creds" | awk '{print $3}')
 
+	# 按 Region 查找样例
 	#regns=("ap-southeast-1"  "ap-northeast-1"  "ap-east-1")
 	#for rgn in ${regns[@]}
 	#do
@@ -42,4 +44,5 @@ do
 	sleep 1
 done
 
-#python3 parse-outputs.py
+# 解析输出，并发送通知信息到 Lark
+python3 parse-outputs.py
